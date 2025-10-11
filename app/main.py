680146bb31ad5c19ac8b7534acaad735568b9f09
Nahid Hasan <nahidhasan9613@gmail.com>
2025-10-11 10:50:59 +0600

    add style.css file, update templates & main file

--- app/main.py
+++ app/main.py
@@ -1,11 +1,18 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from .routers import instrumental_remover, dj_tag_remover
 
-app = FastAPI(
-    title="Music Profanity Remover API",
-    description="APIs for removing profanity from music lyrics.",
-    version="1.0.0",
-)
+app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"message": "Welcome to the Music-Profanity-Remover"}
+app.mount("/static", StaticFiles(directory="app/static"), name="static")
+templates = Jinja2Templates(directory="app/templates")
+
+app.include_router(instrumental_remover.router, tags=["Instrumental Remover"])
+app.include_router(dj_tag_remover.router, tags=["DJ Tag Remover"])
+
+
+@app.get("/", include_in_schema=False)
+async def root(request: Request):
+    """Serves the main index page to choose between tools."""
+    return templates.TemplateResponse("index.html", {"request": request})
